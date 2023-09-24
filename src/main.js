8bc2bf614ff6017ab7655a0434e4c218b93a90a4
Daniel M.C <alinedmooner@gmail.com>
2023-09-24 02:55:11 -0500

    🧹 wip:  main

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
 import '/src/style.css'
 
-document.querySelector('#app').innerHTML = /*html*/`
-  <header></header>
-  <main>
-  <div></div>
-  <div></div>
-  </main>
-  <footer></footer>
-`
+document.querySelector('#app').innerHTML = /*html*/``
 
