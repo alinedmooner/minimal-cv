48d2b1efe0c053ebb4d1a3e7c186a9a6304fb611
Daniel M.C <alinedmooner@gmail.com>
2023-10-06 04:43:05 -0500

    ✨ feat: header component

--- src/ui/CvHeader.js
+++ src/ui/CvHeader.js
@@ -1,8 +1,33 @@
 class CvHeader extends HTMLElement {
   constructor() {
     super();
-    console.log("Header.");
+    this.attachShadow({ mode: "open" });
+    this.play();
+  }
+
+  play() {
+    this.shadowRoot.appendChild(markup.content.cloneNode(true));
   }
 }
+
+const markup = document.createElement("template");
+
+markup.innerHTML = `
+  <style>
+  p {
+    color: green;
+    background-color: black;
+    padding: 3rem;
+    font-size: large;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: system-ui, -apple-system;
+  }
+  </style>
+  <div>
+    <p>Hola, soy ManzComponent</p>
+  </div>
+`;
 
 customElements.define("cv-header", CvHeader);
