9b764d07598d4c7252e3db635e77b208d87eef44
Daniel M.C <alinedmooner@gmail.com>
2023-10-07 03:08:41 -0500

    ✨ feat: cv section

--- src/ui/CvHeader.js
+++ src/ui/CvHeader.js
@@ -1,7 +1,27 @@
+const markup = document.createElement("template");
+
+markup.innerHTML = `
+  <style>
+    h1 {
+      color: #f1efef;
+      background: #272829;
+      font-family: "Mona Sans", sans-serif;
+      font-size: large;
+      text-align: center; 
+    }
+  </style>
+  <header>
+    <h1>Daniel Moreno Cruz</h1>
+  </header>
+`;
+
 class CvHeader extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
     this.play();
   }
 
@@ -9,25 +29,5 @@ class CvHeader extends HTMLElement {
     this.shadowRoot.appendChild(markup.content.cloneNode(true));
   }
 }
-
-const markup = document.createElement("template");
-
-markup.innerHTML = `
-  <style>
-  p {
-    color: green;
-    background-color: black;
-    padding: 3rem;
-    font-size: large;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-family: system-ui, -apple-system;
-  }
-  </style>
-  <div>
-    <p>Hola, soy ManzComponent</p>
-  </div>
-`;
 
 customElements.define("cv-header", CvHeader);
